51a4934172e3464dafdfa9ce2c809062a41a2eed
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-15 00:33:04 -0300

    feat(Packages): Adding packages to the project with __init__

---
+++ app/routes/v1/__init__.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from app.routes.v1.auth_routes import router as auth_router
+from app.routes.v1.ping_route import router as ping_router
+
+
+routers = APIRouter(prefix="/v1")
+router_list = [auth_router, ping_router]
+
+for router in router_list:
+    routers.include_router(router)
+
+__all__ = ["routers"]
