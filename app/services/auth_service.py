a4e260ea05a1865c2944c1afeaeae32ccfcf22a1
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-18 19:01:55 -0300

    feat(AuthService): Adding new auth service method to signbg_in in antoher auth service and valdiadete the service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,9 +1,16 @@
 from aiohttp import ClientSession
 
+from app.core.settings import settings
+from app.schemas.auth_schema import SignIn
+
 
 class AuthService:
     def __init__(self, client: ClientSession):
         self.client = client
 
-    def sign_in(self, schema):
-        print(schema)
+    async def sign_in(self, schema: SignIn):
+        url = settings.auth_service_url
+        print("url: ", f"{url}/sign-in")
+        async with self.client.post(f"{url}/sign-in", json=schema.model_dump()) as response:
+            data = await response.json()
+            return data
