511feeb7904e4316a18b0309fa9a8d5f8be64159
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-18 18:58:32 -0300

    feat(UserRoles): ADDING userroles enums for the schemas and objs

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from pydantic import EmailStr
 
 
 class SignIn(BaseModel):
-    email__eq: str
+    email__eq: EmailStr
     password: str
+
+
+class SignInResponse(BaseModel):
+    access_token: str
+    expiration: str
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,9 +1,8 @@
-from typing import List
-
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import EmailStr
 
+from app.core.enums import UserRoles
 from app.schemas.base_schema import ModelBaseInfo
 
 
@@ -16,4 +15,4 @@ class User(BaseUser, ModelBaseInfo):
     model_config = ConfigDict(from_attributes=True)
 
     is_active: bool
-    role: List[str]
+    role: UserRoles
