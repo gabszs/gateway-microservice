20d464eff7b37019f980ef8a8843bc1394113505
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-19 15:39:37 -0300

    refactor(AsyncClient): Moving Async client out of dependecies to allow dynmic imports to other packages while avoind circular import error

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -4,19 +4,16 @@ from aiohttp import ClientSession
 from fastapi import Depends
 
 from app.core.exceptions import AuthError
+from app.core.http_client import get_async_client
+from app.core.object_storage import AsyncMinioManager
 from app.core.security import JWTBearer
+from app.core.settings import settings
 from app.schemas.user_schema import User
 from app.schemas.user_schema import User as UserSchema
 from app.services import AuthService
+from app.services import MinioService
 
-
-async def get_async_client() -> ClientSession:
-    async with ClientSession() as session:
-        yield session
-
-
-async def get_jwt_bearer(client=Depends(get_async_client)):
-    return JWTBearer(client=client)
+async_minio = AsyncMinioManager()
 
 
 async def get_current_user(user_credentials: UserSchema = Depends(JWTBearer())) -> UserSchema:
@@ -29,10 +26,15 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
-async def get_auth_service(client: ClientSession = Depends(get_async_client)):
+async def get_auth_service(client: ClientSession = Depends(get_async_client)) -> AuthService:
     return AuthService(client=client)
+
+
+async def get_save_service() -> MinioService:
+    return MinioService(async_minio, bucket_name=settings.upload_bucket_name)
 
 
 AuthServiceDependency = Annotated[AuthService, Depends(get_auth_service)]
 CurrentUser = Annotated[User, Depends(get_current_user)]
 CurrentActiveUser = Annotated[User, Depends(get_current_active_user)]
+SaveBucket = Annotated[MinioService, Depends(get_save_service)]
--- app/core/object_storage.py
+++ app/core/object_storage.py
@@ -1,70 +1,18 @@
 from io import BytesIO
 from typing import List
 from typing import Optional
+from typing import Union
 
-from minio import Minio
+from aiohttp.client_reqrep import ClientResponse
 from minio.error import S3Error
 from miniopy_async import Minio as AsyncMinio
 
-from app.core.settings import settings
 from app.core.exceptions import ObjectDownloadError
 from app.core.exceptions import ObjectNotFoundError
 from app.core.exceptions import ObjectStorageError
 from app.core.exceptions import ObjectUploadError
-from icecream import ic
-
-class MinioManager:
-    def __init__(
-        self,
-        endpoint: str = settings.minio_endpoint,
-        access_key: str = settings.minio_access_key,
-        secret_key: str = settings.minio_secret_key,
-        secure=False,
-    ) -> None:
-        self.client = Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
-
-    def upload_file(self, bucket_name: str, object_name: str, file_path: str) -> None:
-        try:
-            self.client.fput_object(bucket_name, object_name, file_path)
-        except FileNotFoundError:
-            ic()
-            raise ObjectNotFoundError(f"File {file_path} not found.")
-        except S3Error as e:
-            ic()
-            raise ObjectUploadError(f"Failed to upload object {object_name} to bucket {bucket_name}: {e}")
-
-    def download_file(self, bucket_name: str, object_name: str, file_path: str) -> None:
-        try:
-            self.client.fget_object(bucket_name, object_name, file_path)
-        except S3Error as e:
-            raise ObjectDownloadError(f"Failed to download object {object_name} from bucket {bucket_name}: {e}")
-
-    def download_file_to_memory(self, bucket_name: str, object_name: str) -> Optional[BytesIO]:
-        try:
-            data = self.client.get_object(bucket_name, object_name)
-            return BytesIO(data.read())
-        except S3Error as e:
-            raise ObjectDownloadError(
-                f"Failed to download object {object_name} to memory from bucket {bucket_name}: {e}"
-            )
-
-    def list_objects(self, bucket_name: str) -> List[str]:
-        try:
-            return [obj.object_name for obj in self.client.list_objects(bucket_name)]
-        except S3Error as e:
-            raise ObjectStorageError(f"Failed to list objects in bucket {bucket_name}: {e}")
-
-    def delete_object(self, bucket_name: str, object_name: str) -> None:
-        try:
-            self.client.remove_object(bucket_name, object_name)
-        except S3Error as e:
-            raise ObjectStorageError(f"Failed to delete object {object_name} from bucket {bucket_name}: {e}")
-
-    def bucket_exists(self, bucket_name: str) -> bool:
-        try:
-            return self.client.bucket_exists(bucket_name)
-        except S3Error as e:
-            raise ObjectStorageError(f"Failed to check if bucket {bucket_name} exists: {e}")
+from app.core.http_client import get_async_client
+from app.core.settings import settings
 
 
 class AsyncMinioManager:
@@ -76,6 +24,11 @@ class AsyncMinioManager:
         secure: bool = False,
     ) -> None:
         self.client = AsyncMinio(endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
+
+    async def put_file(
+        self, bucket_name: str, object_name: str, data: Union[bytearray, BytesIO], length: int, content_type: str
+    ) -> None:
+        await self.client.put_object(bucket_name, object_name, data, length, content_type)
 
     async def upload_file(self, bucket_name: str, object_name: str, file_path: str) -> None:
         try:
@@ -118,3 +71,7 @@ class AsyncMinioManager:
             return await self.client.bucket_exists(bucket_name)
         except S3Error as e:
             raise ObjectStorageError(f"Failed to check if bucket {bucket_name} exists: {e}")
+
+    async def download_object(self, bucket_name: str, object_name: str) -> ClientResponse:
+        async for client_session in get_async_client():
+            return await self.client.get_object(bucket_name, object_name, client_session)
