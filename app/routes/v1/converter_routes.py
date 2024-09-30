ae5c6e23b60469120a1ab6a8582f9b1dd2724d19
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-30 18:08:09 -0300

    feat(QueuasCHEAM): ADDING emailstr typoing

--- app/routes/v1/converter_routes.py
+++ app/routes/v1/converter_routes.py
@@ -5,6 +5,7 @@ from fastapi import File
 from fastapi import status
 from fastapi import UploadFile
 from fastapi.responses import StreamingResponse
+from pydantic import EmailStr
 
 from app.core.dependencies import CurrentUser
 from app.core.dependencies import SaveBucket
@@ -15,10 +16,10 @@ router = APIRouter(prefix="/converter", tags=["Converter"])
 fileUpload = Annotated[UploadFile, File(description="A file read as UploadFile")]
 
 
-@router.post("/upload", status_code=status.HTTP_204_NO_CONTENT)
+@router.post("/upload/{email}", status_code=status.HTTP_204_NO_CONTENT)
 @authorize(role=[UserRoles.MODERATOR, UserRoles.BASE_USER])
-async def upload(file: fileUpload, service: SaveBucket, current_user: CurrentUser):
-    await service.upload_video_file(file)
+async def upload(email: EmailStr, file: fileUpload, service: SaveBucket, current_user: CurrentUser):
+    await service.upload_video_file(file, client_email=email)
 
 
 @router.get("/download", response_class=StreamingResponse)
