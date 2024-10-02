67da8018d60ce3439bc72454a02e45ed8d9b020b
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-10-02 14:53:57 -0300

    refactor(ConverterService): Renaming the service to ConverterService and also refactoring the service class to new ObjectStorage helper class

--- app/services/converter_service.py
+++ app/services/converter_service.py
@@ -1,46 +1,52 @@
-from io import BytesIO
+import os
 
 import pika
 from fastapi import UploadFile
 from fastapi.responses import StreamingResponse
 from pika.adapters.blocking_connection import BlockingChannel
+from pydantic import EmailStr
 
 from app.core.exceptions import BadRequestError
-from app.core.object_storage import AsyncMinioManager
+from app.core.object_storage import AsyncS3Manager
 from app.core.settings import settings
 from app.schemas.file_schema import QueueMessage
 
 
-class MinioService:
-    def __init__(self, minio: AsyncMinioManager, bucket_name: str, rabbit_channel: BlockingChannel) -> None:
-        self.minio = minio
+class ConverterService:
+    def __init__(self, async_s3: AsyncS3Manager, bucket_name: str, rabbit_channel: BlockingChannel) -> None:
+        self.async_s3 = async_s3
         self.bucket_name = bucket_name
         self.rabbit_channel = rabbit_channel
 
-    async def upload_video_file(self, file: UploadFile) -> None:
-        queue_message = QueueMessage(file_name=file.filename, content_type=file.content_type, mp3_filename=None)
-        data = await file.read()
-
-        await self.minio.put_file(
-            bucket_name=self.bucket_name,
-            object_name=queue_message.file_name,  # type: ignore
-            data=BytesIO(data),
-            length=len(data),
-            content_type=file.content_type,
+    async def upload_video_file(self, file: UploadFile, client_email: EmailStr) -> None:
+        queue_message = QueueMessage(
+            file_name=file.filename, content_type=file.content_type, client_email=client_email, download_link=None
         )
+        data = await file.read()
+        key = os.path.basename(queue_message.file_name)
+
+        try:
+            await self.async_s3.put_object(
+                bucket_name=self.bucket_name,
+                key=key,  # type: ignore
+                data=bytes(data),
+            )
+        except Exception as error:
+            raise error
         await self.publish_message(queue_message.file_name, queue_message)
         # se for retornar, vai ser por que preciso retornar o id desse arqruivo
         # mas como o id e o proprio nome do arquivo, entao podemos usar ele e nao
         # precisamos retornar o id, ----- VOU CHECKAR COM SENIOR PARA VER QUAL A ACAO MAIS LOGICA
 
     async def download_video_file(self, object_name: str):
-        response = await self.minio.download_object(self.bucket_name, object_name=object_name)
-        return StreamingResponse(
-            response.content, media_type=response.headers.get("Content-Type", "application/octet-stream")
-        )
+        response = await self.async_s3.get_object(self.bucket_name, key=object_name)
+        content = response["Content"]
+        content_type = response.get("ContentType", "application/octet-stream")
+
+        return StreamingResponse(bytes(content), media_type=content_type)
 
     async def remove_video_file(self, object_name: str):
-        await self.minio.delete_object(self.bucket_name, object_name)
+        await self.async_s3.delete_object(self.bucket_name, object_name)
 
     async def publish_message(self, object_name: str, queue_message: QueueMessage):
         try:
